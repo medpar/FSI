a53ac64a87a35dbb6a7dd26bf00897376114c905
Alonso <alonso.sandoval@alumnos.uva.es>
2024-10-20 18:40:29 +0200

    Subo memoria y .6 modificado

--- Bloque_2/P1_Entregable/e6.m
+++ Bloque_2/P1_Entregable/e6.m
@@ -1,16 +1,16 @@
-% Parte 1: Sin dithering
-img_no_dither = ind2rgb(masked_image_no_dither, map_no_dither);
-[img1, map1] = rgb2ind(img_no_dither, 8, 'nodither');
+% Parte 1: Aplicar la máscara a la imagen original (enmascarar)
+masked_rgb_img = bsxfun(@times, rgb_img, cast(mask, 'like', rgb_img));
 
-% Parte 2: Con dithering
-img_dither = ind2rgb(masked_image_dither, map_dither);
-[img2, map2] = rgb2ind(img_dither, 8, "dither");
+% Cuantificación sin dithering sobre la imagen enmascarada
+[indexed_image_no_dither_masked, map_no_dither_masked] = rgb2ind(masked_rgb_img, 8, 'nodither');
 
-
-subplot(1, 2, 1);
-imshow(img1, map1);
+subplot(2, 2, 1);
+imshow(indexed_image_no_dither_masked, map_no_dither_masked);
 title('Imagen enmascarada cuantificada (3 bits) sin dithering');
-subplot(1, 2, 2);
-imshow(img2, map2);
-title('Imagen enmascarada cuantificada (3 bits) con dithering');
 
+% Cuantificación con dithering sobre la imagen enmascarada
+[indexed_image_dither_masked, map_dither_masked] = rgb2ind(masked_rgb_img, 8, 'dither');
+
+subplot(2, 2, 2);
+imshow(indexed_image_dither_masked, map_dither_masked);
+title('Imagen enmascarada cuantificada (3 bits) con dithering');
